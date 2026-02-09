21ff7b5b7123abb2fe9020a03d3a7e8f14db193b
Arbaz alam <arbazalam6161@gmail.com>
2026-02-10 00:21:45 +0530

    add new files

---
+++ Maximum.java
@@ -0,0 +1,23 @@
+package ARRAYLIST;
+
+import java.util.*;
+
+public class Maximum {
+    public static void main(String args[]) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(10);
+        list.add(20);
+        list.add(5);
+        list.add(15);
+        list.add(25);
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) > max) {
+                max = list.get(i);
+            }
+        }
+        System.out.println("List: " + list);
+
+        System.out.println("Maximum Element: " + max);
+    }
+}
